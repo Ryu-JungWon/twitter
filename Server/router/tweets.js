6c70433c49537f9b81a227f699c142a469554cc9
Ryu-JungWon <ryuzy@kakao.com>
2023-05-03 11:38:44 +0900

    서버 validation 적용

--- Server/router/tweets.js
+++ Server/router/tweets.js
@@ -1,7 +1,18 @@
 import express from 'express';
 import * as tweetController from '../controller/tweet.js';
+import { body } from 'express-validator';
+import { validate } from '../middleware/validateor.js';
 
 const router = express.Router();
+
+const validateTweet = [
+    body('text')
+        .trim()
+        .isLength({ min: 4 })
+        .withMessage('text는 최소 4자이상 입력하세요!'),
+    validate
+];
+
 
 // GET
 // /tweets?username=:username
@@ -11,13 +22,14 @@ router.get('/', tweetController.getTweets);
 // /tweets/:id
 router.get('/:id', tweetController.getTweet);
 
+// text가 4자이하인 경우 에러처리
 // POST
 // id: Date.now().toString()
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 
 // PUT
 // text만 수정
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 
 // DELETE
 router.delete('/:id', tweetController.deleteTweet);
