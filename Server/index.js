e9bb307c864174d6b7a8843146752bd558b59dcf
Ryu-JungWon <ryuzy@kakao.com>
2023-05-12 13:59:39 +0900

    mongodb 적용

--- Server/index.js
+++ Server/index.js
@@ -5,9 +5,7 @@ import tweetsRouter from "./router/tweets.js";
 import authRouter from "./router/auth.js";
 import { config } from './config.js';
 import { initSocket } from "./connection/socket.js";
-import { sequelize } from "./db/database.js";
-// import { db } from "./db/database.js";
-
+import { connectDB } from "./db/database.js";
 
 const app = express();
 app.use(express.json());
@@ -26,10 +24,7 @@ app.use((error, req, res, next) => {
     res.sendStatus(500);
 });
 
-// db.getConnection().then((connection) => console.log(connection));
-
-sequelize.sync().then(() => {
-    // console.log(client);
+connectDB().then(() => {
     const server = app.listen(config.host.port);
     initSocket(server);
-});
+}).catch(console.error);
